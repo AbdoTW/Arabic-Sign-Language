e96dc85a561dffadc6f5a5b11ba60742edae7aa7
abdo tawfik <abdelrahmantawfik740@gmail.com>
2025-01-18 20:11:26 +0200

    delete some files

--- test.py
+++
@@ -1,7 +0,0 @@
-import arabic_reshaper
-from bidi.algorithm import get_display
-
-word = "مسح"
-reshaped_sentence = arabic_reshaper.reshape(word)
-final_sentence = get_display(reshaped_sentence)
-print(final_sentence)
